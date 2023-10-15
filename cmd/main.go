250e1ee130d80305bea22b4d00ef93f77cd5562d
lozovoyDanil <daniil.lozovyi@nure.ua>
2023-10-15 14:23:23 +0300

    Added the ability to launch the app

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"main.go/internal/controller"
+	"main.go/internal/model"
+	"main.go/internal/repository"
+	"main.go/internal/service"
+)
 
+func main() {
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("an error occured while initializing the config: %s", err)
+	}
+
+	db, err := repository.NewSQLiteDB()
+	if err != nil {
+		logrus.Fatalf("an error occured while opening db: %s", err.Error())
+	}
+	repo := repository.NewRepository(db)
+	services := service.NewServices(repo)
+	handler := controller.NewHandler(services)
+
+	srv := new(model.Server)
+	if err := srv.Run(viper.GetString("port"), handler.InitRoutes()); err != nil {
+		logrus.Fatalf("an error occured while running the server: %s", err)
+	}
+
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
