3ed8a4742d26d700f2784620ffd59825c33c0736
lozovoyDanil <daniil.lozovyi@nure.ua>
2023-10-15 14:25:35 +0300

    Deleted unnecessary test router group

--- internal/controller/handler.go
+++ internal/controller/handler.go
@@ -21,10 +21,6 @@ func (h *Handler) InitRoutes() *gin.Engine {
 		auth.POST("/sign-up", h.signUp)
 		auth.POST("/sign-in", h.signIn)
 	}
-	test := router.Group("/test")
-	{
-		test.GET("/test1", h.test)
-	}
 	/*
 		api := router.Group("/api")
 		{
