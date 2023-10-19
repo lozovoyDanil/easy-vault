f1b7a63469c2fa5fe79420ee7fcd8ad567b54ba5
[200~lozovoyDanilgit config --global user.name lozovoyDanilgit config --global user.email danilozovoy@gmail.comgit config --global user.name lozovoyDanilgit config --global user.email danilozovoy@gmail.com <danilozovoy@gmail.com>
2023-10-19 16:42:55 +0300

    Functions for auth endpoints implemented

--- internal/controller/auth.go
+++ internal/controller/auth.go
@@ -4,15 +4,48 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"main.go/internal/model"
 )
 
 func (h *Handler) signUp(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, map[string]any{
-		"status": "ok",
-	})
+	var user model.User
+	err := ctx.BindJSON(&user)
+	if err != nil {
+		newErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	id, err := h.services.Authorization.CreateUser(user)
+	if err != nil {
+		newErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
 
+	ctx.JSON(http.StatusOK, map[string]any{
+		"id": id,
+	})
+}
+
+type signInInput struct {
+	Username string `json:"login" binding:"required"`
+	Password string `json:"password" binding:"required"`
 }
 
 func (h *Handler) signIn(ctx *gin.Context) {
+	var input signInInput
 
+	err := ctx.BindJSON(&input)
+	if err != nil {
+		newErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.services.Authorization.GenerateToken(input.Username, input.Password)
+	if err != nil {
+		newErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, map[string]any{
+		"token": token,
+	})
 }
