d113f7c3b202c1a8025c9585369447d556182bb2
lozovoyDanil <daniil.lozovyi@nure.ua>
2023-10-30 11:25:45 +0200

    Added signatures of future functions

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -10,17 +10,31 @@ type Authorization interface {
 	GetUser(username, password string) (model.User, error)
 }
 
+type Subscription interface {
+	SubByUserId(id int) (model.Subscription, error)
+	CreateSub(model.Subscription) error
+	UpdateSub(model.Subscription) error
+}
+
 type Unit interface {
 }
 
 type Group interface {
+	SpaceGroups(userId, spaceId int) ([]model.StorageGroup, error)
+	GroupById(userId, spaceId, groupId int) (model.StorageGroup, error)
 }
 
 type Space interface {
+	UserSpaces(id int) ([]model.Space, error)
+	SpaceById(userId, spaceId int) (model.Space, error)
+	CreateSpace(userId int, space model.Space) (int, error)
+	UpdateSpace(userId, spaceId int, input model.UpdateSpaceInput) error
+	DeleteSpace(userId, spaceId int) error
 }
 
 type Repository struct {
 	Authorization
+	Subscription
 	Unit
 	Group
 	Space
@@ -29,8 +43,9 @@ type Repository struct {
 func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: NewAuthSQLite(db),
+		Subscription:  NewSubRepository(db),
 		// Unit:          NewUnitSQLite(db),
-		// Group:         NewGroupSQLite(db),
-		// Space:         NewSpaceSQLite(db),
+		Group: NewGroupSQLite(db),
+		Space: NewSpaceSQLite(db),
 	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -11,17 +11,34 @@ type Authorization interface {
 	ParseToken(token string) (int, error)
 }
 
-type Unit interface {
-}
-
-type Group interface {
+type Subscription interface {
+	SubByUserId(id int) (model.Subscription, error)
+	CreateSub(model.Subscription) error
+	UpdateSub(model.Subscription) error
 }
 
 type Space interface {
+	UserSpaces(id int) ([]model.Space, error)
+	SpaceById(userId, spaceId int) (model.Space, error)
+	CreateSpace(userId int, space model.Space) (int, error)
+	UpdateSpace(userId, spaceId int, space model.UpdateSpaceInput) error
+	DeleteSpace(userId, spaceId int) error
+}
+
+type Group interface {
+	SpaceGroups(userId, spaceId int) ([]model.StorageGroup, error)
+	GroupById(userId, spaceId, groupId int) (model.StorageGroup, error)
+	// CreateGroup()
+	// UpdateGroup()
+	// DeleteGroup()
+}
+
+type Unit interface {
 }
 
 type Service struct {
 	Authorization
+	Subscription
 	Unit
 	Group
 	Space
@@ -30,8 +47,9 @@ type Service struct {
 func NewServices(repo *repository.Repository) *Service {
 	return &Service{
 		Authorization: NewAuthService(repo),
+		Subscription:  NewSubService(repo),
 		// Unit:          NewUnitService(repo),
-		// Group:         NewGroupService(repo),
-		// Space:         NewSpaceService(repo),
+		Group: NewGroupService(repo),
+		Space: NewSpaceService(repo),
 	}
 }
