2246cf2ed22792d7bb256907fd5cef237e740058
lozovoyDanil <daniil.lozovyi@nure.ua>
2023-11-28 16:59:09 +0200

    Added required model for profile update.

--- internal/model/user.go
+++ internal/model/user.go
@@ -7,3 +7,10 @@ type User struct {
 	Password string `json:"password"`
 	Role     string
 }
+
+type UpdateUserInput struct {
+	Id       int
+	FullName *string `json:"name"`
+	Email    *string `json:"email"`
+	Password *string `json:"password"`
+}
