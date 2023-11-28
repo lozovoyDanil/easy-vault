0fd562d3f5bc60e92a77bceffd61adf89c069955
lozovoyDanil <daniil.lozovyi@nure.ua>
2023-11-28 17:00:54 +0200

    Added test file

---
+++ internal/controller/units.go
@@ -0,0 +1,19 @@
+package controller
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) groupUnits(ctx *gin.Context) {
+
+}
+
+func (h *Handler) reservedUnits(ctx *gin.Context) {
+
+}
+
+func (h *Handler) reserveUnit(ctx *gin.Context) {
+	fmt.Println("Получили запрос")
+}
